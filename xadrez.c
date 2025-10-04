f48ca7cc96eccc003b1b869e734c95b9d8945c4c
Bryan <bryanfonsecafernandes@gmail.com>
2025-10-04 00:03:16 +0000

    Commit da torre

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,15 @@ int main() {
     int torre = 5;
     int bispo = 5;
     int rainha = 8;
-    
+    int i;
+
+    printf("--- Movimento da Torre! ---\n");
+    i = 1;
+
+    while (i <= torre){
+        printf("Direita!\n");
+            i++;
+    }
+
     return 0;
 }
