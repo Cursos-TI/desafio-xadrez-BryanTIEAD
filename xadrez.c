9aaafe88942fbed9aee34a94b876d788051f2ec9
Bryan <bryanfonsecafernandes@gmail.com>
2025-10-09 23:08:05 +0000

    Movimento do cavalo

--- xadrez.c
+++ xadrez.c
@@ -23,48 +23,27 @@ return;
 printf("Esquerda!\n");
 moverRainha(casas - 1);
 }
+
+// Movimento do cavalo (Interaçao com o usuário)
+void moverCavalo() {
+    int opcao;
+
+    printf("---Movimento do Cavalo! ---\n");
+    printf("Escolha a direçao:\n");
+    printf("1. Duas pra cima e uma pra direita\n");
+    printf("2. Duas pra cima e uma pra esquerda\n");
+    printf("3. Duas pra baixo e uma pra direita \n");
+    printf("4. Duas pra baixo e uma pra esquerda\n");
+    printf("Opçao:\n");
+    scanf("%d", &opcao);
+
+    printf("Movimento escolhido:\n");
+    
+
+
+
+}
 int main() {
-    int torre = 5; //Quantidade de casas que vai mover.
-    int bispo = 5; //Quantidade de casas que vai mover.
-    int rainha = 8; //Quantidade de casas que vai mover.
-    int i; //variavel para auxiliar os loops!
-
-    printf("--- Movimento da Torre! ---\n");
-    i = 1;
-
-    while (i <= torre){
-        printf("Direita!\n");
-            i++;
-    }
-
-    printf("--- Movimento do Bispo! ---\n");
-    i = 1;
-
-    do
-    {
-        printf("Cima, Direita!\n");
-            i++;
-    } while (i <= bispo);
-    
-    printf("--- Movimento da Rainha! ---\n");
-    i = 1;
-
-    for ( i = i; i <= 8; i++)
-    {
-        printf("Ësquerda!\n");
-    }
-    
-
-    printf("--- Movimento do Cavalo! ---\n");
-
-     int movimentoCompleto = 1; // Controla o movimento em "L"
-
-    while (movimentoCompleto--) 
-    {
-        for (int j = 0; j < 2; j++) {
-            printf("Baixo\n"); // Imprime "Baixo" duas vezes
-        }
-        printf("Esquerda\n"); // Imprime "Esquerda" uma vez
-    }
+   
     return 0;
 }
